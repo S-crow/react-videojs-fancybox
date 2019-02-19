cd4c6946651b61c7e9993099aa18d5c694335356
Yanuar Waskito <yanuar.gws@gmail.com>
2019-02-20 03:53:54 +0700

    Remove VideoContent

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import VideoContent from './content';
 import videojs from 'video.js';
 const $ = require('jquery');
 window.jQuery = $;
